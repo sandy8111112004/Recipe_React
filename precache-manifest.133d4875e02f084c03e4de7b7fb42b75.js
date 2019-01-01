70d7d9366bdf4675f3572ef0ba8693e855cf4097
sandy8111112004 <mingshiuantsai1992@gmail.com>
2019-01-01 01:03:14 -0500

    Updates

---
+++ precache-manifest.133d4875e02f084c03e4de7b7fb42b75.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "00184061fd6f45d87390",
+    "url": "/Recipe_React/static/js/runtime~main.00184061.js"
+  },
+  {
+    "revision": "934b4efcbe7bd82040fb",
+    "url": "/Recipe_React/static/js/main.934b4efc.chunk.js"
+  },
+  {
+    "revision": "3912e494b4cc3a5d3eda",
+    "url": "/Recipe_React/static/js/1.3912e494.chunk.js"
+  },
+  {
+    "revision": "934b4efcbe7bd82040fb",
+    "url": "/Recipe_React/static/css/main.326c6b67.chunk.css"
+  },
+  {
+    "revision": "ce79b89f9e234ca0617fabff3b3ae731",
+    "url": "/Recipe_React/index.html"
+  }
+];
